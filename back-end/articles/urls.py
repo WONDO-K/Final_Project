413e70d677724ac8724d6cf533c9fe0957d035aa
wondo-k <wondok95@naver.com>
2024-05-20 15:16:41 +0900

    [feat] 게시글 좋아요, 각 금융상품 리스트 조회 구현 완료

--- back-end/articles/urls.py
+++ back-end/articles/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ArticleListCreateAPIView, ArticleRetrieveAPIView, CommentCreateAPIView, CommentRetrieveAPIView
+from .views import ArticleListCreateAPIView, ArticleRetrieveAPIView, CommentCreateAPIView, CommentRetrieveAPIView, ArticleLikesAPIView
 
 app_name = 'articles'
 
@@ -7,6 +7,10 @@ urlpatterns = [
     # Articles
     path('articles/', ArticleListCreateAPIView.as_view(), name='article-list-create'),
     path('articles/<int:pk>/', ArticleRetrieveAPIView.as_view(), name='article-retrieve'),
+
+    # Article Likes
+    path('articles/<int:pk>/like/', ArticleLikesAPIView.as_view(), name='article-like'), # {'likes': 'likes'}는 ArticleRetrieveAPIView의 likes 메서드를 호출
+
     # Comments
     path('articles/<int:article_pk>/comments/', CommentCreateAPIView.as_view(), name='comment-create'),
     path('articles/<int:article_pk>/comments/<int:pk>/', CommentRetrieveAPIView.as_view(), name='comment-retrieve'),
