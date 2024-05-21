d187a8818cd61bfdb2f956656ed64832807e589c
wondo-k <wondok95@naver.com>
2024-05-21 11:56:22 +0900

    [fix] 금융 API 정보 변동으로 인한 모델링 수정

--- back-end/products/models/DepositProduct.py
+++ back-end/products/models/DepositProduct.py
@@ -26,8 +26,8 @@ class DepositProductOption(models.Model): # 정기 예금 상품 옵션
     intr_rate_type = models.CharField(max_length=20) # 이자율 종류   
     intr_rate_type_nm = models.CharField(max_length=100) # 이자율 종류명
     save_trm = models.IntegerField() # 저축 기간
-    intr_rate = models.DecimalField(max_digits=5, decimal_places=2) # 이자율
-    intr_rate2 = models.DecimalField(max_digits=5, decimal_places=2) # 이자율2
+    intr_rate = models.DecimalField(max_digits=5, decimal_places=2,null=True, blank=True) # 이자율
+    intr_rate2 = models.DecimalField(max_digits=5, decimal_places=2,null=True, blank=True) # 이자율2
 
     def __str__(self):
         return f"{self.deposit_product .fin_prdt_nm} - {self.intr_rate_type_nm}"
